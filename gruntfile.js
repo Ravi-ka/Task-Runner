f3f03798e263a1866a4c4355610aa894fc201901
Ravisankar.Ka <ka.ravisankar@gmail.com>
2024-01-30 22:45:15 +0530

    grunt-contrib-uglify

--- gruntfile.js
+++ gruntfile.js
@@ -1,1 +1,18 @@
-module.exports = function (grunt) {};
+module.exports = function (grunt) {
+  // 1. Configure the task
+  grunt.initConfig({
+    //   2. Specify tasks
+    uglify: {
+      target: {
+        files: {
+          "dest/js/main.min.js": ["src/js/input1.js", "src/js/*.js"],
+        },
+      },
+    },
+  });
+
+  // 3. Load the libraries
+  grunt.loadNpmTasks("grunt-contrib-uglify");
+  // 4. Setting up tasks
+  grunt.registerTask("default", ["uglify"]);
+};
